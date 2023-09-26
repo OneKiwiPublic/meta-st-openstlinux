d9f0e9b80b9dbddd64a49b48ca148b8e3750ecbf
Van Son <thanh.duong.vs@gmail.com>
2023-09-26 13:27:31 +0700

    update V3.4.tar.gz and bin.tar.gz sohoa

--- recipes-graphics/wayland/weston-init/test.sh
+++ recipes-graphics/wayland/weston-init/test.sh
@@ -8,10 +8,10 @@ fi
 
 echo "1. test led:"
 ls /sys/class/leds
-echo 0 > /sys/class/leds/led-r/brightness
-echo 0 > /sys/class/leds/led-g/brightness
-echo 0 > /sys/class/leds/led-b/brightness
-sleep 1
+#led off
+echo 1 > /sys/class/leds/led-r/brightness
+echo 1 > /sys/class/leds/led-g/brightness
+echo 1 > /sys/class/leds/led-b/brightness
 
 echo "2. test uart:"
 ls /dev/ttySTM*
@@ -42,11 +42,32 @@ else
     amixer -q -D pulse sset Master 30%; aplay /home/root/autumn-in-my-heart.wav &
 fi
 
-#echo "7. test 4G:"
-#/usr/local/linuxapp/bin/reset-4g
-
 echo "7. test ethernet:"
 ifconfig
 
+#led red on
+echo 0 > /sys/class/leds/led-r/brightness
+echo 1 > /sys/class/leds/led-g/brightness
+echo 1 > /sys/class/leds/led-b/brightness
+sleep 1
+
+#led green on
+echo 1 > /sys/class/leds/led-r/brightness
+echo 0 > /sys/class/leds/led-g/brightness
+echo 1 > /sys/class/leds/led-b/brightness
+sleep 1
+
+#led blue on
+echo 1 > /sys/class/leds/led-r/brightness
+echo 1 > /sys/class/leds/led-g/brightness
+echo 0 > /sys/class/leds/led-b/brightness
+sleep 1
+
+#led off
+echo 1 > /sys/class/leds/led-r/brightness
+echo 1 > /sys/class/leds/led-g/brightness
+echo 1 > /sys/class/leds/led-b/brightness
+
+#off audio
 #echo 0 > /sys/class/leds/audio-s0/brightness
 #echo 0 > /sys/class/leds/audio-s1/brightness
